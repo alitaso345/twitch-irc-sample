39baa8780da71f1ec71bdc029326ad3b9b1d616e
alitaso345 <alice.maru345@gmail.com>
2020-05-01 09:48:05 +0900

    set channel name by args

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"os"
 
 	"github.com/kelseyhightower/envconfig"
 	irc "github.com/thoj/go-ircevent"
 )
 
-const channel = "#tkch_taisyou"
 const serverssl = "irc.chat.twitch.tv:6697"
 
 type Config struct {
@@ -16,19 +16,22 @@ type Config struct {
 	Password string
 }
 
+// example：https://www.twitch.tv/mogra
+// go run main.go #mogra
 func main() {
 	var config Config
 	envconfig.Process("TWITCH", &config)
 
 	nick := config.Nick
 	con := irc.IRC(nick, nick)
+
 	con.Password = config.Password
 	con.UseTLS = true
 	con.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	channel := os.Args[1]
 	con.AddCallback("001", func(e *irc.Event) { con.Join(channel) })
-	con.AddCallback("PRIVMSG", func(e *irc.Event) {
-		fmt.Printf("[twitch]%s: %s\n", e.User, e.Arguments[1])
-	})
+	con.AddCallback("PRIVMSG", func(e *irc.Event) { fmt.Printf("[twitch]%s: %s\n", e.User, e.Arguments[1]) })
 	err := con.Connect(serverssl)
 	if err != nil {
 		fmt.Printf("Err %s", err)
